eb4655ae390007c77cbc1bbed8caecfda3b595a2
Isaiah <greatpup1940@gmail.com>
2020-11-11 05:55:15 -0500

    sorted columns and changed some of their data types

--- data/migrations/20201025214843_users_table.js
+++ data/migrations/20201025214843_users_table.js
@@ -1,13 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", function (users) {
-    users.increments("user_id");
+    users.increments();
     users.uuid("unique_id").unique().notNullable();
     users.string("org_name", 128).defaultTo("Family Promise Of Spokane");
     users.string("first_name", 128).notNullable();
     users.string("last_name", 128).notNullable();
     users.string("email").notNullable().unique();
     users.string("password", 128).notNullable();
-    users.string("last_4_digits_of_ssn").notNullable();
+    users.string("pin").notNullable();
     users
       .enu("role", ["staff", "admin", "guest", "public"])
       .notNullable()
